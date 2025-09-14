7b61e55e67e2a850cdd5ea3644604df0b6fd8b5f
alesiapetushok <petusokalesa@gmail.com>
2025-09-14 18:31:31 +0300

    Page contacts updated with workers and their job description (they are also in database for now)

--- FastFox/contacts/models.py
+++ FastFox/contacts/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Workers(models.Model):
+    name = models.CharField("Name", max_length=100)
+    job_description = models.TextField("Job Description")
+    photo = models.ImageField("Photo", upload_to="workers_photos/", null=True, blank=True)
+    phone_number = models.CharField(max_length=20)
+    email = models.EmailField()
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Worker"
+        verbose_name_plural = "Workers"
+
--- FastFox/contacts/views.py
+++ FastFox/contacts/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 
+from contacts.models import Workers
+
+
 # Create your views here.
 
 def get_contacts(request):
-    return render(request, 'contacts/contacts.html')
+    workers = get_workers()
+    return render(request, 'contacts/contacts.html', {'workers': workers})
+
+def get_workers():
+    workers = Workers.objects.all()
+    print(f"WORKERS are here: {workers}")
+    return workers
